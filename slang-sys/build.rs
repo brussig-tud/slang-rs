deec2c3ba35b7093c6f29da34a2ac37ebea9ac32
brussig-tud <39052884+brussig-tud@users.noreply.github.com>
2026-04-23 10:46:36 +0200

    Bump targeted Slang version to 2026.7

--- slang-sys/build.rs
+++ slang-sys/build.rs
@@ -43,7 +43,7 @@ use zip;
 
 /// The *Slang* version this crate is tested against.
 #[allow(dead_code)] // <- we only need this for the download feature, but want to keep it anyway as it's important info
-const SLANG_VERSION: &str = "2026.5.1";
+const SLANG_VERSION: &str = "2026.7";
 
 /// The current Linux platform suffix used in release package names
 #[allow(dead_code)] // <- we only need this for the download feature, and even there only for some Linux releases, but
